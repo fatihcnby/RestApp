61bdc5e785d1a314d9e497296dc07adeec674ec5
fatihcnby <fatihcnby@gmail.com>
2024-05-15 22:21:57 +0300

    gif added

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,44 +4,42 @@ import React from "react";
 const CartPage = () => {
   return (
     <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
-      
       <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
-        
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
             <h1 className=" text-xl font-bold">Sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">79.90₺</h2>
+          <h2 className="font-bold">27.30₺</h2>
           <span className="cursor-pointer">X</span>
         </div>
-        
+
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
             <h1 className=" text-xl font-bold">Sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">79.90₺</h2>
+          <h2 className="font-bold">27.30₺</h2>
           <span className="cursor-pointer">X</span>
         </div>
-        
+
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
             <h1 className=" text-xl font-bold">Sicilian</h1>
             <span>Large</span>
           </div>
-          <h2 className="font-bold">79.90₺</h2>
+          <h2 className="font-bold">27.30₺</h2>
           <span className="cursor-pointer">X</span>
         </div>
       </div>
-      
+
       <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
           <span className="">Ara Toplam (3 Adet)</span>
-          <span className="">81.70₺</span>
+          <span className="">81.90₺</span>
         </div>
         <div className="flex justify-between">
           <span className="">Servis Ücreti</span>
@@ -54,7 +52,7 @@ const CartPage = () => {
         <hr className="my-2" />
         <div className="flex justify-between">
           <span className="">TOPLAM(Vergiler Dahil)</span>
-          <span className="font-bold">81.70₺</span>
+          <span className="font-bold">81.90₺</span>
         </div>
         <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
           ÖDEME YAP
